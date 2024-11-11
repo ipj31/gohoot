11d3ec4827d2375a25830adf3046e48c783c4258
Isaac Johnson <johnsonisaacpeter@gmail.com>
2024-11-10 21:16:36 -0600

    added the user logic to the forms

--- src/cmd/gohoot/main.go
+++ src/cmd/gohoot/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/ipj31/gohoot/internal/database"
+	"github.com/ipj31/gohoot/internal/services"
 	"github.com/ipj31/gohoot/web/templates"
 )
 
@@ -18,27 +21,87 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 	templates.Home().Render(context.Background(), w)
 }
 
-func handleRegisterSubmit(w http.ResponseWriter, r *http.Request) {
-	args := templates.RegisterFormArgs{
-		Email:                r.FormValue("email"),
-		Password:             r.FormValue("password"),
-		ConfirmPassword:      r.FormValue("confirm-password"),
-		ConfirmPasswordError: "Passwords do not match!",
+type RegisterSubmit struct {
+	userService *services.UserService
+}
+
+func (rs *RegisterSubmit) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	confirmPassword := r.FormValue("confirm-password")
+
+	if password != confirmPassword {
+		args := templates.RegisterFormArgs{
+			Email:                email,
+			Password:             password,
+			ConfirmPassword:      "",
+			ConfirmPasswordError: "Passwords do not match!",
+		}
+		templates.RegisterForm(args).Render(context.Background(), w)
+		return
 	}
 
-	templates.RegisterForm(args).Render(context.Background(), w)
+	err := rs.userService.CreateUser(email, password)
+	if err != nil {
+		args := templates.RegisterFormArgs{
+			Email:                email,
+			Password:             password,
+			ConfirmPassword:      "",
+			ConfirmPasswordError: "Passwords do not match!",
+		}
+		templates.RegisterForm(args).Render(context.Background(), w)
+		return
+	}
+
+	fmt.Println("Register successful for", email)
+	w.Header().Add("HX-Redirect", "/")
+}
+
+type LoginSubmit struct {
+	userService *services.UserService
+}
+
+func (uc *LoginSubmit) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	valid, err := uc.userService.VerifyLogin(email, password)
+	// TODO don't love the flow, no differentiation between invalid login and error ocurred
+	if !valid || err != nil {
+		args := templates.LoginFormArgs{
+			Email:         email,
+			Password:      password,
+			PasswordError: "Invalid email/password",
+		}
+		templates.LoginForm(args).Render(context.Background(), w)
+		return
+	}
+
+	fmt.Println("Login successful for", email)
+	w.Header().Add("HX-Redirect", "/")
 }
 
 func main() {
-	_, err := database.NewMongoMatchClient("mongodb://admin:password@localhost:27017")
+	mongoClient, err := database.NewMongoMatchClient("mongodb://admin:password@localhost:27017")
 	if err != nil {
 		panic(err)
 	}
 
-	// http.HandleFunc("/", handleHome)
-	// http.Handle("/login", templ.Handler(templates.Login()))
-	// http.Handle("/register", templ.Handler(templates.Register()))
-	// http.HandleFunc("/register-submit", handleRegisterSubmit)
+	userService := services.NewUserService(mongoClient)
 
-	// http.ListenAndServe("", nil)
+	registerSubmitRoute := &RegisterSubmit{
+		userService: userService,
+	}
+
+	loginSubmitRoute := &LoginSubmit{
+		userService: userService,
+	}
+
+	http.HandleFunc("/", handleHome)
+	http.Handle("/login", templ.Handler(templates.Login()))
+	http.Handle("/register", templ.Handler(templates.Register()))
+	http.Handle("/register-submit", registerSubmitRoute)
+	http.Handle("/login-submit", loginSubmitRoute)
+
+	http.ListenAndServe("", nil)
 }
